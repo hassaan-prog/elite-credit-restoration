63f551381bd5ee850b475792a1a3e894838d23ed
hassaan-prog <hassaan.ahmed@bigimmersive.com>
2019-12-16 17:57:05 +0500

    Updates

---
+++ precache-manifest.c097d2b5ccbac1520cd8c795ad9b5ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51543cef170d1bb638a56b8b6b7946eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05e719b2092fdc570e9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ffb7478df911333fdb7",
+    "url": "/static/js/2.9c8d70fd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9c8d70fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c05e719b2092fdc570e9",
+    "url": "/static/js/main.c8cd2d06.chunk.js"
+  },
+  {
+    "revision": "2a44b914c232fe7088c9",
+    "url": "/static/js/runtime-main.b8902395.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
